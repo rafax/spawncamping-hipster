0156391f4a8b2ca708f9f89fa4613381ea883e6b
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2014-07-24 20:09:03 +0200

    Use a iterator instead of loading input into memory

--- problems/src/com/gajdulewicz/problems/ThreadsReadParagraphWithQueues.java
+++ problems/src/com/gajdulewicz/problems/ThreadsReadParagraphWithQueues.java
@@ -3,10 +3,7 @@ package com.gajdulewicz.problems;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.SynchronousQueue;
 
@@ -15,14 +12,13 @@ import java.util.concurrent.SynchronousQueue;
  */
 public class ThreadsReadParagraphWithQueues {
 
-    private final List<String> inputWords;
-    private volatile int index = 0;
+    private Iterator<String> input;
     private final int threadCount;
-    private final List<SynchronousQueue<List<String>>> queues;
+    private final List<SynchronousQueue<Iterator<String>>> queues;
 
 
-    public ThreadsReadParagraphWithQueues(Collection<String> input, int threadCount) {
-        inputWords = new LinkedList<>(input);
+    public ThreadsReadParagraphWithQueues(Iterator<String> input, int threadCount) {
+        this.input = input;
         this.threadCount = threadCount;
         queues = new ArrayList<>();
         for (int i = 0; i < threadCount; i++) {
@@ -35,7 +31,7 @@ public class ThreadsReadParagraphWithQueues {
             new Thread(new Printer(queues.get(i), queues.get((i + 1) % threadCount)), Integer.toString(i)).start();
         }
         try {
-            queues.get(0).put(inputWords);
+            queues.get(0).put(input);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -46,20 +42,20 @@ public class ThreadsReadParagraphWithQueues {
         new ThreadsReadParagraphWithQueues(getInput(), 10).startPrinter();
     }
 
-    private static List<String> getInput() {
+    private static Iterator<String> getInput() {
         try {
-            return Files.readAllLines(Paths.get("/usr/share/dict/words"));
+            return Files.lines(Paths.get("/usr/share/dict/words")).iterator();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return new ArrayList<>();
+        return new ArrayList<String>().iterator();
     }
 
     private static class Printer implements Runnable {
-        private final BlockingQueue<List<String>> input;
-        private final BlockingQueue<List<String>> output;
+        private final BlockingQueue<Iterator<String>> input;
+        private final BlockingQueue<Iterator<String>> output;
 
-        private Printer(BlockingQueue<List<String>> input, BlockingQueue<List<String>> output) {
+        private Printer(BlockingQueue<Iterator<String>> input, BlockingQueue<Iterator<String>> output) {
             this.input = input;
             this.output = output;
         }
@@ -67,9 +63,9 @@ public class ThreadsReadParagraphWithQueues {
         @Override
         public void run() {
             try {
-                List<String> words = input.take();
-                while (!words.isEmpty()) {
-                    String firstWord = words.remove(0);
+                Iterator<String> words = input.take();
+                while (words.hasNext()) {
+                    String firstWord = words.next();
                     System.out.println(String.format("Thread %s printing: %s", Thread.currentThread().getName(), firstWord));
                     output.put(words);
                     words = input.take();

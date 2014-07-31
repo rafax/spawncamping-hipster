cebc4342ee5774fe95793ce933744c4e1bfb6816
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2014-07-31 08:34:46 +0200

    Queue with two stacks, avoiding copying

---
+++ problems/src/com/gajdulewicz/problems/QueueWithTwoStacks.java
@@ -0,0 +1,52 @@
+package com.gajdulewicz.problems;
+
+import org.junit.Test;
+
+import java.util.Stack;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by rafal on 31/07/14.
+ */
+public class QueueWithTwoStacks<T> {
+
+    private Stack<T> left = new Stack<>();
+    private Stack<T> right = new Stack<>();
+    int size = 0;
+
+    public void push(T elem) {
+        size++;
+        left.push(elem);
+    }
+
+    public T pop() {
+        if (right.size() == 0) {
+            for (int i = 0; i < size; i++) {
+                right.push(left.pop());
+            }
+        }
+        size--;
+        return right.pop();
+
+
+    }
+
+    @Test
+    public void isFifo() {
+        QueueWithTwoStacks<Integer> q = new QueueWithTwoStacks<>();
+        for (int i = 0; i < 200; i++) {
+            q.push(i);
+        }
+        for (int i = 0; i < 100; i++) {
+            assertEquals(Integer.valueOf(i), q.pop());
+        }
+        for (int i = 200; i < 300; i++) {
+            q.push(i);
+        }
+        for (int i = 100; i < 300; i++) {
+            assertEquals(Integer.valueOf(i), q.pop());
+        }
+    }
+
+}

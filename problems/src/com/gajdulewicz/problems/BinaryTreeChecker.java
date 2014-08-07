1a4f41f81747394a611b9167589ae39aa0d41ebf
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2014-08-07 19:30:00 +0200

    Iterator version of binary tree checker

--- problems/src/com/gajdulewicz/problems/BinaryTreeChecker.java
+++ problems/src/com/gajdulewicz/problems/BinaryTreeChecker.java
@@ -3,6 +3,9 @@ package com.gajdulewicz.problems;
 import com.gajdulewicz.ds.trees.Entry;
 import org.junit.Test;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
 
@@ -13,6 +16,68 @@ public class BinaryTreeChecker {
 
     static boolean isBinaryTree(Entry<Integer, Integer> root) {
         return check(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    static boolean isBinaryTreeIterator(Entry<Integer, Integer> root) {
+        final Iterator<Integer> integerIterator = inOrderIterator(root);
+        if (!integerIterator.hasNext()) {
+            return true;
+        }
+        int prev = integerIterator.next();
+        while (integerIterator.hasNext()) {
+            int curr = integerIterator.next();
+            if (prev > curr) {
+                return false;
+            }
+            prev = curr;
+        }
+        return true;
+    }
+
+    static Iterator<Integer> inOrderIterator(Entry<Integer, Integer> root) {
+        return new Iterator<Integer>() {
+
+            boolean initialized = false;
+            Entry<Integer, Integer> next = root;
+
+
+            @Override
+            public boolean hasNext() {
+                return next != null;
+            }
+
+            @Override
+            public Integer next() {
+                if (!hasNext()) throw new NoSuchElementException();
+                if (!initialized) {
+                    while (next.getLeftChild() != null) {
+                        next = next.getLeftChild();
+                    }
+                    initialized = true;
+                }
+                Integer result = next.getKey();
+                findNext();
+                return result;
+            }
+
+            private void findNext() {
+                if (next.getRightChild() != null) {
+                    next = next.getRightChild();
+                    while (next.getLeftChild() != null) {
+                        next = next.getLeftChild();
+                    }
+                } else {
+                    while (next.getParent() != null) {
+                        if (next.getParent().getLeftChild() == next) {
+                            next = next.getParent();
+                            return;
+                        }
+                        next = next.getParent();
+                    }
+                    next = next.getParent();
+                }
+            }
+        };
     }
 
     private static boolean check(Entry<Integer, Integer> root, int minValue, int maxValue) {
@@ -45,13 +110,13 @@ public class BinaryTreeChecker {
 
     @Test
     public void rootNull() {
-        assertTrue(BinaryTreeChecker.isBinaryTree(null));
+        assertTrue(BinaryTreeChecker.isBinaryTreeIterator(null));
     }
 
     @Test
     public void justRoot() {
         Entry<Integer, Integer> root = new Entry<>(1, null);
-        assertTrue(BinaryTreeChecker.isBinaryTree(root));
+        assertTrue(BinaryTreeChecker.isBinaryTreeIterator(root));
     }
 
     @Test
@@ -61,7 +126,7 @@ public class BinaryTreeChecker {
         root.getLeftChild().setParent(root);
         root.setRightChild(new Entry<>(3, null));
         root.getRightChild().setParent(root);
-        assertTrue(BinaryTreeChecker.isBinaryTree(root));
+        assertTrue(BinaryTreeChecker.isBinaryTreeIterator(root));
     }
 
     @Test
@@ -71,7 +136,7 @@ public class BinaryTreeChecker {
         root.getLeftChild().setParent(root);
         root.setRightChild(new Entry<>(1, null));
         root.getRightChild().setParent(root);
-        assertFalse(BinaryTreeChecker.isBinaryTree(root));
+        assertFalse(BinaryTreeChecker.isBinaryTreeIterator(root));
 
     }
 
@@ -84,7 +149,7 @@ public class BinaryTreeChecker {
         root.getLeftChild().getRightChild().setParent(root.getLeftChild());
         root.setRightChild(new Entry<>(3, null));
         root.getRightChild().setParent(root);
-        assertFalse(BinaryTreeChecker.isBinaryTree(root));
+        assertFalse(BinaryTreeChecker.isBinaryTreeIterator(root));
     }
 
     @Test
@@ -96,7 +161,7 @@ public class BinaryTreeChecker {
         root.getLeftChild().getRightChild().setParent(root.getLeftChild());
         root.setRightChild(new Entry<>(5, null));
         root.getRightChild().setParent(root);
-        assertFalse(BinaryTreeChecker.isBinaryTree(root));
+        assertFalse(BinaryTreeChecker.isBinaryTreeIterator(root));
     }
 
 

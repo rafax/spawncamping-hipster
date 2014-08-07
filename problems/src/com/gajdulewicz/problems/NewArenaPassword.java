ef966991357ad842ab46bfaddcaf017bd9989f57
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2014-08-07 07:55:46 +0200

    New arena pass

---
+++ problems/src/com/gajdulewicz/problems/NewArenaPassword.java
@@ -0,0 +1,62 @@
+package com.gajdulewicz.problems;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by rafal on 06/08/14.
+ */
+public class NewArenaPassword {
+
+    public int minChange(String oldPassword, int K) {
+        int n = oldPassword.length();
+        int res = 0;
+        for (int i = 0; i < n - K && i < K; i++) {
+            int[] counts = new int[26];
+            int t = 0;
+            // The cells that must be equal:
+            for (int j = i; j < n; j += n - K) {
+                // Count the number of times each character happens
+                counts[oldPassword.charAt(j) - 'a']++;
+                t++;
+            }
+            // If we pick the character that appears the most times
+            // Then we need t - (count of the character) to convert the other
+            // positions to it.
+            res += t - maxElem(counts);
+        }
+        return res;
+
+    }
+
+    private int maxElem(int[] counts) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] > max) {
+                max = counts[i];
+            }
+        }
+        return max;
+    }
+
+    @Test
+    public void verify1() {
+        assertEquals(3, new NewArenaPassword().minChange("topcoderopen", 5));
+    }
+
+    @Test
+    public void verify2() {
+        assertEquals(0, new NewArenaPassword().minChange("puyopuyo", 4));
+    }
+
+    @Test
+    public void verify3() {
+        assertEquals(1, new NewArenaPassword().minChange("loool", 3));
+    }
+
+    @Test
+    public void verify4() {
+        assertEquals(5, new NewArenaPassword().minChange("amavckdkz", 7));
+    }
+}

11372b803bc962da4b119f1911e4cd8e88272c4a
Gajdulewicz, Rafal <Rafal.Gajdulewicz@vimn.com>
2016-06-06 09:06:41 +0200

    Less memory

--- problems/src/main/java/com/gajdulewicz/spawncamping/hipster/problems/LongestSubstring.java
+++ problems/src/main/java/com/gajdulewicz/spawncamping/hipster/problems/LongestSubstring.java
@@ -1,7 +1,9 @@
 package com.gajdulewicz.spawncamping.hipster.problems;
 
-import java.util.*;
-import java.util.stream.IntStream;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 
 public class LongestSubstring {
 
@@ -10,11 +12,12 @@ public class LongestSubstring {
         if (args.length > 0) {
             limit = Integer.parseInt(args[0]);
         }
-        //Set<String> sub = longestSubstringDynamic("abab", "baba");
         String a = longString(limit) + "ggg" + longString(limit);
         String b = longString(limit) + "ggg" + longString(limit);
+        long start = System.currentTimeMillis();
         Set<String> sub = longestSubstringDynamic(a, b);
-        System.out.println(sub);
+        long end = System.currentTimeMillis();
+        System.out.println("Found in " + (end - start) + " " + sub);
     }
 
     private static String longString(Integer limit) {
@@ -26,26 +29,28 @@ public class LongestSubstring {
     }
 
     private static Set<String> longestSubstringDynamic(String a, String b) {
-        int[][] len = new int[a.length()][b.length()];
+        int[][] len = new int[2][b.length()];
         int maxLen = 0;
         Set<String> maxes = new HashSet<>();
         for (int i = 0; i < a.length(); i++) {
             for (int j = 0; j < b.length(); j++) {
                 if (a.charAt(i) == b.charAt(j)) {
                     if (i == 0 || j == 0) {
-                        len[i][j] = 1;
+                        len[1][j] = 1;
                     } else {
-                        len[i][j] = len[i - 1][j - 1] + 1;
-                        if (len[i][j] > maxLen) {
-                            maxLen = len[i][j];
+                        len[1][j] = len[0][j - 1] + 1;
+                        if (len[1][j] > maxLen) {
+                            maxLen = len[1][j];
                             maxes.clear();
                         }
-                        if (len[i][j] == maxLen && maxLen <= i + 1) {
+                        if (len[1][j] == maxLen && maxLen <= i + 1) {
                             maxes.add(a.substring(i - maxLen + 1, i + 1));
                         }
                     }
                 }
             }
+            System.arraycopy(len[1], 0, len[0], 0, len[0].length);
+            Arrays.fill(len[1], 0);
         }
         return maxes;
     }
